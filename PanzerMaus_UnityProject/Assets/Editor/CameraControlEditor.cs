027994f93da32aa45814436f89fc7ed0f110880e
Lee Whittaker <lee@21zero.com>
2015-02-27 16:58:45 -0700

    Zoom Controls working

---
+++ PanzerMaus_UnityProject/Assets/Editor/CameraControlEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(CameraControl))]
+public class CameraControlEditor : Editor {
+
+	private ControlMode controlMode;
+
+	public override void OnInspectorGUI(){
+		CameraControl cameraControl = (CameraControl)target;
+
+		controlMode = (ControlMode)EditorGUILayout.EnumPopup("Control Mode", controlMode); 
+
+		switch (controlMode){
+		case ControlMode.mouse: 
+			EditorGUILayout.LabelField("Mouse Controls");
+			cameraControl.wheel = EditorGUILayout.FloatField("  Wheel Sensitivity", cameraControl.wheel);
+			cameraControl.mouseDragSpeed = EditorGUILayout.FloatField("  Pan Speed", cameraControl.mouseDragSpeed);
+			break;
+		case ControlMode.touch:
+			EditorGUILayout.LabelField("Touch Screen Controls");
+			cameraControl.pinch = EditorGUILayout.FloatField("  Pinch Sensitivity", cameraControl.pinch);
+			cameraControl.fingerDragSpeed = EditorGUILayout.FloatField("  Pan Speed", cameraControl.fingerDragSpeed);
+			break;
+		default:
+			break;
+		}
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField ("Camera Field of View Limits");
+		EditorGUILayout.LabelField ("Minimum:", cameraControl.minFoV.ToString()); 
+		EditorGUILayout.LabelField ("Maximum:", cameraControl.maxFoV.ToString());
+		EditorGUILayout.MinMaxSlider (ref cameraControl.minFoV, ref cameraControl.maxFoV, 0.0f, 100.0f);
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField ("Camera Position Limits");
+		cameraControl.min = EditorGUILayout.Vector2Field("Bottom Left", cameraControl.min);
+		cameraControl.max = EditorGUILayout.Vector2Field("Top Right", cameraControl.max);
+	}
+}
+
+public enum ControlMode{
+	mouse,
+	touch,
+}
---
+++ PanzerMaus_UnityProject/Assets/_Scripts/CameraControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float wheel, pinch, mouseDragSpeed, fingerDragSpeed, minFoV, maxFoV;
+	public Vector2 min, max;
+
+	private float z;
+
+
+	public void Update()
+	{
+#if UNITY_WEBPLAYER
+		float scroll = Input.GetAxis ("Mouse ScrollWheel"); 
+		if(!camera.isOrthoGraphic){
+			camera.fieldOfView = Mathf.Clamp (camera.fieldOfView - (scroll * wheel),
+			                                  minFoV,
+		    	                              maxFoV);
+		} else {
+			camera.orthographicSize = Mathf.Clamp (camera.orthographicSize - (scroll * wheel),
+			                                       minFoV,
+			                                       maxFoV);
+		}
+
+#endif
+
+#if UNITY_IOS
+		if(Input.touches == 2){
+			Touch one = Input.GetTouch(0);
+			Touch two = Input.GetTouch(1);
+			
+			Vector2 onePrev = one.position - one.deltaPosition;
+			Vector2 twoPrev = two.position - two.deltaPosition;
+			
+			float oldMag = (onePrev - twoPrev).magnitude;
+			float newMag = (one.position - two.position).magnitude;
+			
+			float magDiff = oldMag - newMag;
+			if (!camera.isOrthoGraphic){
+				camera.fieldOfView = Mathf.Clamp (camera.fieldOfView - (magDiff * pinch),
+				                                  minFoV,
+				                                  maxFoV);
+			}else{
+				camera.orthographicSize = Mathf.Clamp (camera.orthographicSize - (magDiff * pinch),
+				                                       minFoV,
+				                                       maxFoV);
+			}
+		}
+#endif
+	}
+
+}
