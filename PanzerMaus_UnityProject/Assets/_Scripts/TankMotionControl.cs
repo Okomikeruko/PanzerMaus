aea32ac41be68713c0b6f57c8412f68b6c669d77
Lee Whittaker <whittakerlee81@gmail.com>
2015-03-11 17:20:50 -0600

    Exploding Floors and Tank Movement

---
+++ PanzerMaus_UnityProject/Assets/_Scripts/TankMotionControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMotionControl : MonoBehaviour {
+
+	public bool movement = false;
+	public float speed, maxSpeed;
+	void Update()
+	{
+		float move = Input.GetAxis("Horizontal");
+		if(movement & rigidbody2D.velocity.magnitude < maxSpeed){
+			rigidbody2D.AddRelativeForce(new Vector2(
+				move,
+				-Mathf.Abs (move / 5)) * speed, ForceMode2D.Force);
+		}
+	}
+}
