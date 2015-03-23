4f5bfa8f235831c63b081310ab4c1f05e31d5535
Lee Whittaker <whittakerlee81@gmail.com>
2015-03-23 11:59:45 -0600

    Enhanced inner angle turning

--- PanzerMaus_UnityProject/Assets/_Scripts/TankMotionControl.cs
+++ PanzerMaus_UnityProject/Assets/_Scripts/TankMotionControl.cs
@@ -5,17 +5,22 @@ public class TankMotionControl : MonoBehaviour {
 
 	public bool movement = false;
 	private int stopper = 1;
-	public float speed, maxSpeed, groundDistance, rotationalSpeed, rotationalTolerance, gravitationalForce, forwardDistance, rearDistance;
-
+	public float speed, maxSpeed, groundDistance, angularDistance, rotationalSpeed, rotationalTolerance, gravitationalForce, forwardDistance, rearDistance;
+	public Vector2 offset;
+	private Vector3 offsetTransform;
 	private float move;
 	private bool turn = false;
 	private Vector3 gravity;
 	private GameObject climber;
 	private RaycastHit2D down, forwardDown, backwardDown, forward, backward;
-
+	
 	void Update()
 	{
-		move = Input.GetAxis("Horizontal");
+		offsetTransform = new Vector3 (transform.position.x + offset.x,
+		                               transform.position.y + offset.y,
+		                               transform.position.z);
+
+		move = 1; //Input.GetAxis("Horizontal");
 		if(movement & rigidbody2D.velocity.magnitude < maxSpeed){
 			rigidbody2D.AddForce(transform.right * move * stopper * speed, ForceMode2D.Force);
 		}
@@ -26,13 +31,17 @@ public class TankMotionControl : MonoBehaviour {
 		forward = getDirection (transform.right);
 		backward = getDirection (-transform.right);
 
+		gravity = (down.distance < groundDistance * ((turn) ? 2 : 1)) ? new Vector3(-down.normal.x, -down.normal.y, 0) : Vector3.down;
+
 		Debug.DrawRay (transform.position, -transform.up);
 		Debug.DrawRay (transform.position, (-transform.up + transform.right).normalized);
 		Debug.DrawRay (transform.position, (-transform.up + -transform.right).normalized);
-		Debug.DrawRay (transform.position, transform.right);
-		Debug.DrawRay (transform.position, -transform.right);
+		Debug.DrawRay (offsetTransform, transform.right);
+		Debug.DrawRay (offsetTransform, -transform.right);
 
 		float sqrt = down.distance * Mathf.Sqrt(2) + rotationalTolerance;
+
+		/* Outide Corner Turn */
 
 		if (sqrt <= forwardDown.distance && move > 0 && forwardDistance < forward.distance && !turn || 
 		    sqrt <= backwardDown.distance && move < 0 && rearDistance < backward.distance && !turn){
@@ -40,29 +49,28 @@ public class TankMotionControl : MonoBehaviour {
 				stopper = 0;
 				transform.Rotate(0, 0, -move * rotationalSpeed);
 		}else{
+			if(!turn){
+				rigidbody2D.AddForce(gravity * gravitationalForce, ForceMode2D.Force);
+			}
 			stopper = 1;
 		}
 
+		/* Inside Corner Turn */
 		if (forward.distance < forwardDistance && move > 0 || 
 		    backward.distance < rearDistance && move < 0){
 			turn = true;
 		}
 
-		if (forward.distance > forwardDistance * 3 && move > 0 ||
-		    backward.distance > rearDistance * 3 && move < 0){
+		if (forward.distance > forwardDistance * angularDistance && move > 0 ||
+		    backward.distance > rearDistance * angularDistance && move < 0){
 			turn = false;
 		}
 
-		Debug.Log (turn);
 
-
-
-		gravity = (down.distance < groundDistance) ? new Vector3(-down.normal.x, -down.normal.y, 0) : Vector3.down;
-		rigidbody2D.AddForce(gravity * gravitationalForce, ForceMode2D.Force);
 
 		if (turn){
-			rigidbody2D.velocity = Vector2.zero;
-			stopper = 0;
+//			rigidbody2D.velocity = Vector2.zero;
+//			stopper = 0;
 			transform.Rotate(0, 0, move * rotationalSpeed);
 		}
 	}
