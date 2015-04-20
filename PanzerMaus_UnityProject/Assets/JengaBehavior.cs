42a042da6a5b54ee4a907b6fe7af75415bc6720c
Lee Whittaker <whittakerlee81@gmail.com>
2015-04-20 12:43:20 -0600

    Updated Jenga Scripts

--- PanzerMaus_UnityProject/Assets/JengaBehavior.cs
+++ PanzerMaus_UnityProject/Assets/JengaBehavior.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class JengaBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject line;
+	public Team team;
+
+	private bool unsubscribed = false;
+	void Start(){
+		switch (team) {
+		case Team.teamOne:
+			JengaVictoryEvent.jengaOne += empty;
+			break;
+		case Team.teamTwo:
+			JengaVictoryEvent.jengaTwo += empty;
+			break;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if (!unsubscribed && transform.position.y < line.transform.position.y) {
+			unsubscribed = true;
+			switch (team) {
+			case Team.teamOne:
+				JengaVictoryEvent.jengaOne -= empty;
+				break;
+			case Team.teamTwo:
+				JengaVictoryEvent.jengaTwo -= empty;
+				break;
+			}
+		}
 	}
+
+	void empty (){}
 }
---
+++ PanzerMaus_UnityProject/Assets/JengaVictoryEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class JengaVictoryEvent : MonoBehaviour {
+	public delegate void MyDelegate();
+	public static event MyDelegate jengaOne, jengaTwo; 
+
+	void Update (){
+		if (jengaOne.GetInvocationList ().Length <= 0) {
+			Debug.Log("Player Two Wins");
+		}
+		if (jengaTwo.GetInvocationList ().Length <= 0) {
+			Debug.Log ("Player One Wins");
+		}
+	}
+}
+
+ public enum Team{
+	teamOne,
+	teamTwo
+}
